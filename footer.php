b747f85728b7b814133db3fef03d26112bea4e70
Rakesh David <rakesh@maslow.ai>
2021-08-16 11:24:37 -0400

    Uploading HTML
    Uploading all the HTML files exported from Webflow to repo

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer id="footer" class="footer section-4 section-8">
+    <div class="footer-flex-container">
+      <a href="#" class="footer-logo-link w-inline-block"><img src="images/Maslow-Complete-Logo-Black.png" width="126" height="23" sizes="(max-width: 479px) 126px, (max-width: 767px) 19vw, 126px" srcset="images/Maslow-Complete-Logo-Black-p-500.png 500w, images/Maslow-Complete-Logo-Black-p-800.png 800w, images/Maslow-Complete-Logo-Black-p-1080.png 1080w, images/Maslow-Complete-Logo-Black-p-1600.png 1600w, images/Maslow-Complete-Logo-Black.png 1700w" alt="" class="footer-image"></a>
+      <div class="div-block-7">
+        <div class="text-block-47"><span class="text-span-12">New Jersey</span><br><span class="text-span-13">12345 Blvd of Americas<br>New York City, NY 10001.<br>Opening Hours 9am-5pm EST</span></div>
+        <a href="#" class="button footer w-button">CONTACT US</a>
+      </div>
+      <div class="div-block-8">
+        <div class="text-block-48"><span class="text-span-12">India</span><br><span class="text-span-13">123 S ave<br>Hyderabad, India.<br>Opening Hours 9am-5pm IST</span></div>
+        <a href="#" class="button footer w-button">CONTACT US</a>
+      </div>
+      <div>
+        <div class="text-block-46"><span class="text-span-14">Copyright 2020</span><br><span class="text-span-15">All rights reserved</span></div>
+      </div>
+    </div>
+  </footer>
